f6b52b00a2ad6ecc8a53962e3e2ad8c1b607a96f
DevLobato <dev.lobatovinicius@gmail.com>
2023-11-09 09:00:52 -0300

    feat: (server) Initial setup backend

---
+++ server/src/server.ts
@@ -0,0 +1,19 @@
+import fastify from 'fastify'
+import { PrismaClient } from '@prisma/client'
+
+const app = fastify()
+const prisma = new PrismaClient()
+
+app.get('/orders', async () => {
+  const users = await prisma.user.findMany()
+
+  return users
+})
+
+app
+  .listen({
+    port: 31000,
+  })
+  .then(() => {
+    console.log('Http server running on http://localhost:31000')
+  })
