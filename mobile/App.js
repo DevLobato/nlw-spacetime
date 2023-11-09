e3d9935ea11f5939ec58d21c9dbf082e440a4fe2
DevLobato <dev.lobatovinicius@gmail.com>
2023-11-09 14:41:23 -0300

    feat: (Web & Mobile) Configuring Mobile

--- mobile/App.js
+++ mobile/App.js
@@ -3,18 +3,11 @@ import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View className="bg-gray-950 flex-1 items-center justify-center">
+      <Text className="text-gray-50 font-bold text-5xl">
+        NLW Spacetime | 2023
+      </Text>
+      <StatusBar style="light" translucent/>
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
